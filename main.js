abfc45d8726a8a5cee3bc4a1f53e7cc1fa68d0ff
inhwa1025 <inhwa9901@gmail.com>
2020-12-06 14:42:14 +0900

    Reserve Create

--- main.js
+++ main.js
@@ -9,12 +9,21 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
 
     if(pathname === '/'){
-      if(queryData.id === undefined){
-        reserve.home(request, response);
-      } else {
-        reserve.page(request, response);
-      }
-    } else {
+      reserve.home(request, response);
+    } 
+    else if(pathname === '/create_process'){
+      topic.create_process(request, response);
+    } 
+    else if(pathname === '/update'){
+      topic.update(request, response);
+    } 
+    else if(pathname === '/update_process'){
+      topic.update_process(request, response);
+    } 
+    else if(pathname === '/delete_process'){
+      topic.delete_process(request, response);
+    } 
+    else {
       response.writeHead(404);
       response.end('Not found');
     }
